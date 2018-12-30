ad42ad41e5c65f41a33d8c8ae43a884e33677b9d
Muhammad Qasim <ins_sheikh@hotmail.com>
2018-12-30 03:37:20 +0000

    business logic

---
+++ CQRS.Business/Utils/Results.cs
@@ -0,0 +1,27 @@
+﻿namespace CQRS.Business.Utils
+{
+    public enum State
+    {
+        ok,
+        badRequest,
+        notFound,
+        unProcessableEntity,
+        noContent
+    }
+
+    public class Results
+    {
+        public State Status { get; private set; }
+        public bool IsSuccessful { get; private set; }
+        public dynamic ResponseMessage { get; private set; }
+
+        public Results SetResponse(State state, bool isSuccessful, dynamic response)
+        {
+            return new Results {
+                 Status = state,
+                 IsSuccessful = isSuccessful,
+                 ResponseMessage = response
+            };
+        }
+    }
+}
